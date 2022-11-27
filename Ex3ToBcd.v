5ee92d70d1edce4cc6a28501a325f89734688d97
Md Sajjad Hossain <81988769+mdsajjadhossain25@users.noreply.github.com>
2022-11-27 19:56:45 +0600

    Update Ex3ToBcd.v
    new addition

--- Ex3ToBcd.v
+++ Ex3ToBcd.v
@@ -1,7 +1,8 @@
-module Ex3toBcd(input [3:0]e, output [3:0]b);
- 	
-	assign b[0] = ~e[0];
-	assign b[1] = e[1]^e[0];
-	assign b[2] = (~e[2]&~e[1]) | (~e[2]&~e[0]) | (e[2]&e[1]&e[0]);
-	assign b[3] = (e[3]&e[1]&e[0]) | (e[3]&e[2]);
+module Ex3_to_BCD(input [3:0]e, output[3:0]b );
+	wire p;
+	assign b[0]=~e[0];
+	xor(p, e[1], e[0]);
+	assign b[1]=p;
+	assign b[2]= (~e[2]&~e[1])|(e[2]&e[1]&e[0])|(~e[2]&~e[0]);
+	assign b[3]= (e[3]&e[2])|(e[3]&e[1]&e[0]);
 endmodule
